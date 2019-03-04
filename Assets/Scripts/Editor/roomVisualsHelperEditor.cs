5fbbd546a8ea886e8398da1317ee37bbebacae7e
Anderson Addo <34724755+andersonaddo@users.noreply.github.com>
2019-03-04 11:47:23 +0000

    Room can now transition between themes properly! Can also load themes into roomVisualsHelper and apply them on room in Editor

--- Assets/Scripts/Editor/roomVisualsHelperEditor.cs
+++ Assets/Scripts/Editor/roomVisualsHelperEditor.cs
@@ -7,6 +7,7 @@ public class roomVisualsHelperEditor : Editor
 {
 
     SerializedProperty materialChangersList, lightChangersList;
+    roomVisualsHelper helper;
 
     void OnEnable()
     {
@@ -18,7 +19,7 @@ public class roomVisualsHelperEditor : Editor
     public override void OnInspectorGUI()
     {
         serializedObject.Update(); //set serialized object based on what is in the actual class (in case I've changed things from the last frame)
-        roomVisualsHelper helper = (roomVisualsHelper)target;
+        helper = (roomVisualsHelper)target;
 
         //Showing original script opener
         GUI.enabled = false;
@@ -59,50 +60,96 @@ public class roomVisualsHelperEditor : Editor
 
         if (GUILayout.Button("Save Theme"))
         {
-            EditorUtility.DisplayDialog("Saving New Room Visual",
-                                        "Go through all your changers. Only the ones that are expanded will be saved as configurations in this saved holder",
-                                        "Go Ahead",
-                                        "Cancel");
-
-            //Credit to http://wiki.unity3d.com/index.php/CreateScriptableObjectAsset for the below code
-            roomVisualsHolder newHolder =  ScriptableObject.CreateInstance<roomVisualsHolder>();
-
-            foreach (roomVisualsLightsChanger lightChanger in helper.changers.Where ((lightChanger) => lightChanger.isExpanded))
-            {
-                roomLightConfiguration lightConfig = new roomLightConfiguration();
-                lightConfig.targetClassification = lightChanger.targetClassification;
-                lightConfig.realTimeLightColor = lightChanger.testRealTimeLightColor;
-                lightConfig.range = lightChanger.testRange;
-                lightConfig.intensity = lightChanger.testIntensity;
-                newHolder.lightConfigurations.Add(lightConfig);
-            }
-
-            
-            foreach (roomVisualsMaterialChanger matChanger in helper.materialChangers.Where ((mChanger) => mChanger.isExpanded))
-            {
-                roomMaterialConfiguration matConfig = new roomMaterialConfiguration();
-                matConfig.mat = matChanger.mat;
-                matConfig.emissMatColor = matChanger.testMainEmissMatColor;
-                newHolder.materialConfigurations.Add(matConfig);
-            }
-
-
-            string path = "Assets";
-            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(roomVisualsHolder).ToString() + ".asset");
-            AssetDatabase.CreateAsset(newHolder, assetPathAndName);
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-            EditorUtility.FocusProjectWindow();
-            Selection.activeObject = newHolder;
+            saveVisuals();
         }
 
 
         if (GUILayout.Button("Load Theme"))
         {
-            EditorUtility.OpenFilePanel("Select a theme", "Assets", "asset");
+            loadVisuals();
         }
 
         serializedObject.ApplyModifiedProperties(); //take what is in the object stream and put it in the class (so the the class itself is updated with the changes made here
         Repaint();
+    }
+
+
+
+    void saveVisuals()
+    {
+        if (!EditorUtility.DisplayDialog("Saving New Room Visual",
+        "Go through all your changers. Only the ones that are expanded will be saved as configurations in this saved holder",
+                                        "Go Ahead",
+                                        "Cancel")
+        ) return;
+
+        //Credit to http://wiki.unity3d.com/index.php/CreateScriptableObjectAsset for the below code
+        roomVisualsHolder newHolder = ScriptableObject.CreateInstance<roomVisualsHolder>();
+
+        foreach (roomVisualsLightsChanger lightChanger in helper.changers.Where((lightChanger) => lightChanger.isExpanded))
+        {
+            roomLightConfiguration lightConfig = new roomLightConfiguration();
+            lightConfig.targetClassification = lightChanger.targetClassification;
+            lightConfig.realTimeLightColor = lightChanger.testRealTimeLightColor;
+            lightConfig.range = lightChanger.testRange;
+            lightConfig.intensity = lightChanger.testIntensity;
+            newHolder.lightConfigurations.Add(lightConfig);
+        }
+
+
+        foreach (roomVisualsMaterialChanger matChanger in helper.materialChangers.Where((mChanger) => mChanger.isExpanded))
+        {
+            if (matChanger.mat == null) continue;
+            roomMaterialConfiguration matConfig = new roomMaterialConfiguration();
+            matConfig.mat = matChanger.mat;
+            matConfig.emissMatColor = matChanger.testMainEmissMatColor;
+            newHolder.materialConfigurations.Add(matConfig);
+        }
+
+
+        string path = "Assets";
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New " + typeof(roomVisualsHolder).ToString() + ".asset");
+        AssetDatabase.CreateAsset(newHolder, assetPathAndName);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = newHolder;
+    }
+
+    void loadVisuals()
+    {
+        string path = EditorUtility.OpenFilePanel("Select a theme", "Assets", "asset");
+
+        //Making path relative to Assets folder
+        if (path.StartsWith(Application.dataPath))
+            path = "Assets" + path.Substring(Application.dataPath.Length);
+
+        roomVisualsHolder loadedHolder = AssetDatabase.LoadAssetAtPath<roomVisualsHolder>(path);
+        if (loadedHolder == null) return;
+
+        foreach (roomLightConfiguration lightConfig in loadedHolder.lightConfigurations)
+        {
+            roomVisualsLightsChanger lightChanger = helper.changers
+                .Where(changer => changer.targetClassification == lightConfig.targetClassification)
+                .FirstOrDefault();
+
+            if (lightChanger == null) continue;
+            lightChanger.testIntensity = lightConfig.intensity;
+            lightChanger.testRealTimeLightColor = lightConfig.realTimeLightColor;
+            lightChanger.testRange = lightConfig.range;
+            helper.testRealtimeLights(lightChanger);
+        }
+
+
+        foreach (roomMaterialConfiguration matConfig in loadedHolder.materialConfigurations)
+        {
+            roomVisualsMaterialChanger matChanger = helper.materialChangers
+                .Where(changer => changer.mat == matConfig.mat)
+                .FirstOrDefault();
+
+            if (matChanger == null) continue;
+            matChanger.testMainEmissMatColor = matConfig.emissMatColor;
+            helper.testMaterialEmission(matChanger);
+        }
     }
 }
--- Assets/Scripts/Room Theming/roomVisualsChanger.cs
+++ Assets/Scripts/Room Theming/roomVisualsChanger.cs
@@ -1,8 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class roomVisualsChanger : MonoBehaviour
 {
+    public float completionTime; 
+    float stopTime;
 
+    public List<roomVisualsHolder> roomThemes = new List<roomVisualsHolder>();
+    int currentIndex;
+    List<LightChangeDeltas> lightDeltas = new List<LightChangeDeltas>();
+    List<MaterialChangeDeltas> matDeltas = new List<MaterialChangeDeltas>();
+
+
+    //Should be called in update for timing to be correct
+    void transitionTo(roomVisualsHolder newHolder)
+    {
+        lightDeltas.Clear();
+        matDeltas.Clear();
+        foreach(roomLightConfiguration lightConfig in newHolder.lightConfigurations)
+        {
+            LightClass sampleLightClass = FindObjectsOfType<LightClass>().Where(light => light.classification == lightConfig.targetClassification).FirstOrDefault();
+            if (sampleLightClass != null) lightDeltas.Add(new LightChangeDeltas(lightConfig, sampleLightClass.light));
+        }
+
+        foreach (roomMaterialConfiguration matConfig in newHolder.materialConfigurations)
+        {
+           matDeltas.Add(new MaterialChangeDeltas(matConfig));
+        }
+
+        //Changing will start on next update()
+        stopTime = Time.time + completionTime;
+    }
+
+    void Update()
+    {
+        if (stopTime >= Time.time)
+        {
+            foreach(LightChangeDeltas lightDelta in lightDeltas)
+            {
+                foreach (LightClass lightClass in FindObjectsOfType<LightClass>().Where(light => light.classification == lightDelta.lightClassification))
+                {
+                    lightDelta.applyChange(lightClass.light, completionTime);
+                }
+            }
+
+            foreach (MaterialChangeDeltas matDelta in matDeltas)
+                matDelta.applyChange(completionTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentIndex++;
+            if (currentIndex == roomThemes.Count) currentIndex = 0;
+            transitionTo(roomThemes[currentIndex]);
+        }
+    }
+}
+
+public struct LightChangeDeltas
+{
+    public LightClass.lightClassifications lightClassification;
+    float deltaRange, deltaIntensity;
+    float deltaR, deltaG, deltaB;
+    roomLightConfiguration newConfig;
+
+    public LightChangeDeltas(roomLightConfiguration newConfig, Light sampleLight)
+    {
+        this.newConfig = newConfig;
+        lightClassification = newConfig.targetClassification;
+        deltaIntensity = Mathf.Abs(newConfig.intensity - sampleLight.intensity);
+        deltaRange = Mathf.Abs(newConfig.range - sampleLight.range);
+        deltaR = Mathf.Abs(newConfig.realTimeLightColor.r - sampleLight.color.r);
+        deltaG = Mathf.Abs(newConfig.realTimeLightColor.g - sampleLight.color.g);
+        deltaB = Mathf.Abs(newConfig.realTimeLightColor.b - sampleLight.color.b);
+    }
+
+    public void applyChange(Light light, float completionTime)
+    {
+        light.intensity = Mathf.MoveTowards(light.intensity, newConfig.intensity, deltaIntensity * Time.deltaTime / completionTime);
+        light.range = Mathf.MoveTowards(light.range, newConfig.range, deltaRange * Time.deltaTime / completionTime);
+        float r = Mathf.MoveTowards(light.color.r, newConfig.realTimeLightColor.r, deltaR * Time.deltaTime / completionTime);
+        float g = Mathf.MoveTowards(light.color.g, newConfig.realTimeLightColor.g, deltaG * Time.deltaTime / completionTime);
+        float b = Mathf.MoveTowards(light.color.b, newConfig.realTimeLightColor.b, deltaB * Time.deltaTime / completionTime);
+        light.color = new Color(r, g, b);
+    }
+
+}
+
+
+public struct MaterialChangeDeltas
+{
+    float deltaEmR, deltaEmG, deltaEmB;
+    roomMaterialConfiguration newConfig;
+
+    public MaterialChangeDeltas(roomMaterialConfiguration newConfig)
+    {
+        this.newConfig = newConfig;
+        deltaEmR = Mathf.Abs(newConfig.emissMatColor.r - newConfig.mat.GetColor("_EmissionColor").r);
+        deltaEmG = Mathf.Abs(newConfig.emissMatColor.g - newConfig.mat.GetColor("_EmissionColor").g);
+        deltaEmB = Mathf.Abs(newConfig.emissMatColor.b - newConfig.mat.GetColor("_EmissionColor").b);
+    }
+
+    public void applyChange(float completionTime)
+    {
+        Color emColor = newConfig.mat.GetColor("_EmissionColor");
+        float r = Mathf.MoveTowards(emColor.r, newConfig.emissMatColor.r, deltaEmR * Time.deltaTime / completionTime);
+        float g = Mathf.MoveTowards(emColor.g, newConfig.emissMatColor.g, deltaEmG * Time.deltaTime / completionTime);
+        float b = Mathf.MoveTowards(emColor.b, newConfig.emissMatColor.b, deltaEmB * Time.deltaTime / completionTime);
+        newConfig.mat.SetColor("_EmissionColor", new Color(r, g, b));
+    }
 }
